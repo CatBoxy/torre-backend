54f97d92a38be601721711a08b892d878ece479d
Juan <juancruzlambrechts@negsoluciones.com>
2023-04-18 18:53:51 -0300

    added dynamic route

--- app.js
+++ app.js
@@ -1,18 +1,16 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT;
-app.use(express.static("public"));
 const cors = require("cors");
+const userApi = require("./routes/api/user");
 
+app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors());
 
-// API Routes
-const userApi = require("./routes/api/user");
-
-// API URLs
 app.use("/api/user", userApi);
+
 app.use(function (req, res, next) {
   res.status(404).json({
     status: "error 400",
--- routes/api/user.js
+++ routes/api/user.js
@@ -2,19 +2,20 @@ const express = require("express");
 const router = express.Router();
 const axios = require("axios");
 
-const userData = async (req, res) => {
+const getUserData = async (req, res) => {
+  const username = req.params.username;
   axios
-    .get("https://torre.bio/api/bios/juancruzlambrechts")
+    .get(`https://torre.bio/api/bios/${username}`)
     .then((response) => {
       console.log(response.data);
       res.send(response.data);
     })
     .catch((error) => {
       console.log(error);
-      res.send(error);
+      res.status(404).send(error);
     });
 };
 
-router.get("/", userData);
+router.get("/:username", getUserData);
 
 module.exports = router;
